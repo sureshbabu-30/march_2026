5fcb2b6f18206676a21964a1df0791871ba09b3c
sureshbabu-30 <job.ksb@gmail.com>
2026-03-11 11:48:41 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,36 @@
+#Q1
+# tuples = ("vihaan",3,"pink")
+# print(tuples)
+
+#Q2
+# day = ('monday','tuesday','wednesday','thursday','friday','saturday','sunday')
+# print(day[4])
+
+#Q3
+# list_1 = (1,3,5)
+# list_2 = (2,4,6)
+# print(list_1+list_2)
+
+#Q4
+# length = 15
+# width = 5
+# area_rectangle = length*width
+# print(area_rectangle)
+
+#Q5
+# tuples = (1,2,6,5)
+# result = 10 in tuples
+# print(result)
+
+#Q6
+# items = [("Apple", 99), ("Banana", 99), ("Milk", 49)]
+# print(f"Item\tPrice")
+# print("-"*20)
+# total = 0
+# for i,j in items:
+#     print(f"{i}\t{float(j)}")
+#     total += j
+
+# print("-"*20)
+# print(f"Total\t{float(total)}")
+
